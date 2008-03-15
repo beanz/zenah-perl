33a9921ad8f4b78796f94a5b7e31b4ab9fcfadf4
Mark Hindess <soft-github@temporalanomaly.com>
2008-03-15 15:20:43 +0000

    Removing debug.

--- lib/ZenAH/Engine/Plugin/xPL.pm
+++ lib/ZenAH/Engine/Plugin/xPL.pm
@@ -91,9 +91,6 @@ sub add {
         };
     }
   }
-  foreach (sort keys %filter) {
-    print "  K: $_ => ".$filter{$_}."\n";
-  }
 
   $self->{_engine}->add_xpl_callback(id => 'trigger-for-rule-'.$rule,
                                      targetted => 0,
@@ -152,7 +149,7 @@ Mark Hindess, E<lt>zenah@beanz.uklinux.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006, 2007 by Mark Hindess
+Copyright (C) 2007 by Mark Hindess
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
