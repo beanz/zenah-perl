40e86968aba05d9251d33f9fce814395b6e671d6
Mark Hindess <soft-github@temporalanomaly.com>
2008-03-24 21:55:15 +0000

    Support for adding data with a scale on the right of a graph.

--- lib/ZenAH/Google/Chart.pm
+++ lib/ZenAH/Google/Chart.pm
@@ -4,56 +4,77 @@ use warnings;
 use base qw(Class::Accessor);
 use POSIX qw/log10 ceil floor strftime/;
 __PACKAGE__->mk_accessors(qw(width height
-                             title data type colours legend
-                             data_labels min max xfmt xskip xmod fill));
+                             title type colours
+                             data min max legend
+                             right_data right_min right_max right_legend
+                             xfmt xskip xmod fill));
 
 sub url {
   my $self = shift;
   my $base = 'http://chart.apis.google.com/chart?';
   my @args = ();
+  my $has_right_data = defined $self->right_data;
   push @args, 'cht='.$self->type;
   push @args, 'chs='.$self->width.'x'.$self->height;
-  push @args, 'chd='.$self->data_simple;
+  my @simple_data = ();
+  my $g =
+    $self->data_simple($self->min, $self->max, $self->data, \@simple_data);
+  my $right_g =
+    $self->data_simple($self->right_min, $self->right_max, $self->right_data,
+                       \@simple_data) if ($has_right_data);
+  push @args, 'chd=s:'.(join ',', @simple_data);
   my $colours = $self->colours;
   push @args, 'chco='.(join ',',@{$colours}) if (ref $colours &&
                                                  scalar @{$colours});
-  push @args, 'chxt=x,y';
-  push @args, 'chxl=0:|'.(join '|', @{$self->x_labels}).'|'.
-                   '1:|'.(join '|', @{$self->y_labels});
+  my $xt = 'x,y';
+  $xt .= ',r' if ($has_right_data);
+  push @args, 'chxt='.$xt;
+  my $xl = '0:|'.(join '|', @{$self->x_labels}).'|'.
+           '1:|'.(join '|', @{$self->y_labels($g)});
+  $xl .= '|2:|'.(join '|', @{$self->y_labels($right_g)})if ($has_right_data);
+  push @args, 'chxl='.$xl;
   push @args, $self->fill if ($self->fill);
-  my $legend = $self->legend;
-  push @args, 'chdl='.(join '|', @$legend) if ($legend);
+  my @legend = ();
+  push @legend, @{$self->legend};
+  push @legend, @{$self->right_legend} if ($has_right_data);
+  push @args, 'chdl='.(join '|', @legend) if (scalar @legend);
   return $base.(join '&', @args);
 }
 
 sub data_simple {
   my $self = shift;
-  my $min = $self->min;
-  my $max = $self->max;
-  my %ds = ();
-  foreach my $d (@{$self->data}) {
+  my $min = shift;
+  my $max = shift;
+  my $data = shift;
+  my $results = shift || [];
+  my @ds = ();
+  foreach my $d (@{$data}) {
     my ($x, @v) = @$d;
     my $i = 0;
     foreach (@v) {
-      push @{$ds{$i}}, $_;
+      push @{$ds[$i]}, $_;
       $i++;
       next unless (defined $_);
       $max = $_ if (!defined $max || $max < $_);
       $min = $_ if (!defined $min || $min > $_);
     }
   }
-  my $g = $self->{_g} = loose_label($min, $max);
+  my $g = loose_label($min, $max);
+  _simple_encode($g->{gmin}, $g->{gmax}, \@ds, $results);
+  return $g;
+}
+
+sub _simple_encode {
+  my ($min, $max, $data_sets, $results) = @_;
   my @enc = ('A'..'Z', 'a' .. 'z', '0'..'9');
-  my $scale = ($g->{gmax}-$g->{gmin})/61;
-  my @s = ();
-  my $gmin = $g->{gmin};
-  foreach my $i (sort { $a <=> $b } keys %ds) {
-    my $values = $ds{$i};
-    push @s, join '', map {
-      defined $_ ? $enc[int(($_-$gmin)/$scale)] : '_'
+  my $scale = ($max-$min)/61;
+  $results = [] unless ($results);
+  foreach my $values (@$data_sets) {
+    push @$results, join '', map {
+      defined $_ ? $enc[int(($_-$min)/$scale)] : '_'
     } @$values;
   }
-  return 's:'.(join ',', @s);
+  return $results;
 }
 
 sub x_labels {
@@ -94,7 +115,7 @@ sub x_labels {
 
 sub y_labels {
   my $self = shift;
-  my $g = $self->{_g};
+  my $g = shift;
   return [ map { sprintf $g->{stepfmt}, $_ } @{$g->{ticks}} ];
 }
 
