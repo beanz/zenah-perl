f5eaf84a7b45980c3eaa06de34172d739a39a09c
Mark Hindess <soft-github@temporalanomaly.com>
2009-12-15 23:33:03 +0000

    Remove DefaultEnd usage.

--- lib/ZenAH.pm
+++ lib/ZenAH.pm
@@ -10,7 +10,7 @@ use warnings;
 # Static::Simple: will serve static files from the application's root
 # directory
 #
-use Catalyst qw/-Debug ConfigLoader FormValidator DefaultEnd SubRequest/;
+use Catalyst qw/-Debug ConfigLoader FormValidator SubRequest/;
 
 our $VERSION = '0.01';
 
